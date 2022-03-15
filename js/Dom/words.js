6831f227cfb76841b35e7eda4e2e5a0c296cb827
amandam2017 <amandamaarman19911@gmail.com>
2022-03-15 12:50:53 +0200

    set-up pages

---
+++ js/Dom/words.js
@@ -0,0 +1,5 @@
+// reference elements: 
+// input field to get the value or the entered sentence
+// add btn to do something onclick
+const sentenceElem = document.querySelector('.sentence');
+const addBtnElem = document.querySelector('.addBtn');
